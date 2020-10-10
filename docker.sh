da5b305c8c3f3b55a6267c8f6704f5b414c4e17e
zhouzhihui <peregrinator@yeah.net>
2020-10-10 21:59:21 +0800

    chore: configure to change the database root user administration password at will

--- docker.sh
+++ docker.sh
@@ -11,23 +11,24 @@ sleep 3
 echo `service mysql status`
 
 #判断是否初始化数据，如果初始化了，直接启动。
+echo '2.开始导入数据....'
 if [ ! -d /var/lib/mysql/blogs ] ; then
-    echo '2.开始导入数据....'
     #导入数据
     sed -i 's/ADMIN_USER_NAME/'$ADMIN_USER_NAME'/' /mysql/blogs.sql
     sed -i 's/ADMIN_USER_PASSWORD/'$ADMIN_USER_PASSWORD'/' /mysql/blogs.sql
     mysql < /mysql/blogs.sql
-    echo '3.导入数据完毕....'
-
-    sleep 3
-    echo `service mysql status`
-
-    #重新设置mysql密码
-    echo '4.开始修改密码....'
-    sed -i 's/MYSQL_ROOT_PASSWORD/'$MYSQL_ROOT_PASSWORD'/' /mysql/privileges.sql
-    mysql < /mysql/privileges.sql
-    echo '5.修改密码完毕....'
 fi
+
+echo '3.导入数据完毕....'
+
+sleep 3
+echo `service mysql status`
+
+#重新设置mysql密码
+echo '4.开始修改密码....'
+sed -i 's/MYSQL_ROOT_PASSWORD/'$MYSQL_ROOT_PASSWORD'/' /mysql/privileges.sql
+mysql < /mysql/privileges.sql
+echo '5.修改密码完毕....'
 
 #sleep 3
 echo `service mysql status`
